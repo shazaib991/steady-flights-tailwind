eaca5bc2070fcf40b49e5b94e61c62b6e7e4f4b2
shazaib991 <shazaib424647@gmail.com>
2022-12-07 23:17:41 +0500

    fixed fonts in form section

--- components/LoginAndRegister/FormSection.js
+++ components/LoginAndRegister/FormSection.js
@@ -24,11 +24,17 @@ export const FormSection = ({ isRegisterPage }) => {
           <div
             className={
               isRegisterPage
-                ? "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[12px] mt-[60px]"
+                ? "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[10px] mt-[60px]"
                 : "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[12px] mt-[80px]"
             }
           >
-            <h1 className="text-[20px] mb-[10px] text-[#212529] font-['customFontExtraBold']">
+            <h1
+              className={
+                isRegisterPage
+                  ? "text-[20px] mb-[6px] text-[#212529] font-['customFontExtraBold']"
+                  : "text-[20px] mb-[10px] text-[#212529] font-['customFontExtraBold']"
+              }
+            >
               <span className="text-[#1ABEC8]">
                 {isRegisterPage ? "Register" : "Login"}
               </span>{" "}
@@ -36,7 +42,7 @@ export const FormSection = ({ isRegisterPage }) => {
                 ? "to fly away with us!"
                 : "and let's explore world!"}
             </h1>
-            <p className="text-[12px] mb-[30px] font-['customFontMedium']">
+            <p className="text-[12px] mb-[27px] font-['customFontMedium']">
               {isRegisterPage
                 ? "Flights you need, at prices you'll love."
                 : "At prices you'll love."}
@@ -50,7 +56,8 @@ export const FormSection = ({ isRegisterPage }) => {
                   type="text"
                   name="Email"
                   id="Email"
-                  className="w-full text-[13px] text-[#212529] border-none outline-none mt-[7px] font-['customFontSemiBold']"
+                  autoComplete="off"
+                  className="w-full text-[13px] text-[#212529] border-none outline-none mt-[5px] font-['customFontBold']"
                 />
               </div>
               <div className="w-full h-[61px] border border-solid border-[#D3D3D3] rounded-[15px] px-[15px] flex justify-between items-center mt-[17px]">
@@ -62,7 +69,8 @@ export const FormSection = ({ isRegisterPage }) => {
                     type="text"
                     name="Password"
                     id="Password"
-                    className="w-full text-[13px] text-[#212529] border-none outline-none mt-[7px] font-['customFontMedium']"
+                    autoComplete="off"
+                    className="w-full text-[13px] text-[#212529] border-none outline-none mt-[5px] font-['customFontBold']"
                   />
                 </div>
                 <Image
@@ -75,11 +83,11 @@ export const FormSection = ({ isRegisterPage }) => {
               </div>
             </form>
             {isRegisterPage ? (
-              <div className="text-[12px] text-[#7A7C7F] inline-block mt-[11px] mb-[20px]">
+              <div className="text-[12px] text-[#7A7C7F] inline-block mt-[11px] mb-[17px] font-['customFontMedium']">
                 Password must be at least 8 characters.
               </div>
             ) : (
-              <div className="text-[12px] inline-block cursor-pointer my-[12px] tracking-[0.2px]">
+              <div className="text-[12px] inline-block cursor-pointer my-[12px] tracking-[0.2px] font-['customFontBold']">
                 Reset Password
               </div>
             )}
@@ -94,17 +102,17 @@ export const FormSection = ({ isRegisterPage }) => {
                     <div className="w-[4.3px] h-[10px] absolute top-px left-[5px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
                     <div className="w-[4.3px] h-[10px] absolute top-[1.4px] left-[5.4px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
                   </div>
-                  <p className="text-[14px] cursor-default ml-[5px]">
+                  <p className="text-[14px] cursor-default ml-[5px] font-['customFontMedium']">
                     Subscribe to Newsletter
                   </p>
                 </div>
               </div>
             )}
             <div className="flex flex-col">
-              <button className="w-full h-[44px] bg-[#1ABEC8] text-white text-[14px] border-none rounded-[11px] mb-[10px] tracking-[0.2px]">
+              <button className="w-full h-[44px] bg-[#1ABEC8] text-white text-[14px] border-none rounded-[11px] mb-[10px] tracking-[0.2px] font-['customFontMedium']">
                 {isRegisterPage ? "Register" : "Login"} with Steady Flights
               </button>
-              <button className="w-full h-[44px] bg-white text-[14px] border-none rounded-[11px] flex justify-center items-center text-[#0000008A] shadow-[0px_2px_3px_0px_#0000002B] mb-[20px]">
+              <button className="w-full h-[44px] bg-white text-[14px] border-none rounded-[11px] flex justify-center items-center text-[#0000008A] shadow-[0px_2px_3px_0px_#0000002B] mb-[20px] font-['customFontBold']">
                 <Image
                   src="/assets/logo googleg 48dp.png"
                   alt="google authorization"
@@ -115,7 +123,7 @@ export const FormSection = ({ isRegisterPage }) => {
                 {isRegisterPage ? "Register" : "Login"} with Google
               </button>
             </div>
-            <p className="text-[12px] tracking-[0.2px]">
+            <p className="text-[12px] tracking-[0.2px] font-['customFontBold']">
               Don&apos;t have an account?{" "}
               <span
                 className="text-[#1ABEC8] cursor-pointer"
