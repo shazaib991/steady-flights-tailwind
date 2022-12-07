438e8531ab7d316ab7d44abb3a9b5f9fc6be671e
shazaib991 <shazaib424647@gmail.com>
2022-12-07 19:10:40 +0500

    added login and signup footer

--- components/LoginAndRegister/Footer.js
+++ components/LoginAndRegister/Footer.js
@@ -2,55 +2,99 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <div className="{styles.footerCover}">
-      <div className="{styles.footerWrapper}">
-        <div className="{styles.footerContainer}">
-          <div className="{styles.footerLogo}">
+    <div className="w-full bg-[#212529] flex justify-center">
+      <div className="w-full flex justify-center max-w-[1366px]">
+        <div className="w-[74%] mt-[104px] mb-[80px] flex justify-between">
+          <div className="mr-[140px]">
             <Image
               src="/assets/JPG-02.png"
               alt="steady flights logo"
               width={120}
               height={40}
+              className="mb-[23px]"
             />
-            <p>Flights you need, at prices you&apos;ll love.</p>
+            <p className="text-[11px] text-white tracking-[0.2px]">
+              Flights you need, at prices you&apos;ll love.
+            </p>
           </div>
-          <div className="{styles.footerRightContainer}">
-            <div className="{styles.footerLinksAndSubscribeContainer}">
-              <div className="{styles.footerLinksContainer}">
-                <div className="{styles.footerLinks}">
-                  <h1>Company</h1>
-                  <p>About us</p>
-                  <p>Career</p>
-                  <p>Blog</p>
-                  <p>Tickets</p>
-                  <p>Pricing</p>
-                  <p>Demo</p>
+          <div className="flex">
+            <div className="flex">
+              <div className="flex">
+                <div className="mr-[50px]">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px]">
+                    Company
+                  </h1>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    About us
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Career
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Blog
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Tickets
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Pricing
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Demo
+                  </p>
                 </div>
-                <div className="{styles.footerLinks}">
-                  <h1>Support</h1>
-                  <p>Help Center</p>
-                  <p>Contact us</p>
-                  <p>Terms & Condition</p>
-                  <p>Sitemap</p>
-                  <p>Feedback</p>
+                <div className="mr-[50px]">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px]">
+                    Support
+                  </h1>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Help Center
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Contact us
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Terms & Condition
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Sitemap
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Feedback
+                  </p>
                 </div>
-                <div className="{styles.footerLinks}">
-                  <h1>Media</h1>
-                  <p>Facebook</p>
-                  <p>Twitter</p>
-                  <p>Youtube</p>
-                  <p>Instagram</p>
+                <div className="mr-[50px]">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px]">
+                    Media
+                  </h1>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Facebook
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Twitter
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Youtube
+                  </p>
+                  <p className="text-[12px] text-[#e3e4e4] mb-[20px] cursor-pointer">
+                    Instagram
+                  </p>
                 </div>
               </div>
-              <div className="{styles.footerSubscribe}">
-                <h1>Subscribe Us</h1>
+              <div className="flex flex-col">
+                <h1 className="text-[14px] text-white mb-[33px]">
+                  Subscribe Us
+                </h1>
                 <input
                   type="text"
                   name="subscribe"
                   id="subscribe"
                   placeholder="Your email here..."
+                  className="w-[250px] h-[61px] text-white rounded-none rounded-[7px] text-[12px] outline-none pl-[25px] pr-[25px] bg-[#373B3E] mb-[15px] placeholder:text-white"
                 />
-                <button>Subscribe</button>
+                <button className="w-[250px] h-[61px] rounded-[7px] text-[14px] bg-white">
+                  Subscribe
+                </button>
               </div>
             </div>
           </div>
