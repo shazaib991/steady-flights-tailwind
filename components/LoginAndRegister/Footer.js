ed61a620055fa45402942f176a5a326331d42eb2
shazaib991 <shazaib424647@gmail.com>
2022-12-08 18:57:54 +0500

    added responsiveness in footer

--- components/LoginAndRegister/Footer.js
+++ components/LoginAndRegister/Footer.js
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <div className="w-full bg-[#212529] flex justify-center">
       <div className="w-full flex justify-center max-w-[1366px]">
-        <div className="w-[74%] mt-[104px] mb-[80px] flex justify-between">
-          <div className="mr-[140px]">
+        <div className="w-[74%] mt-[104px] mb-[80px] flex justify-between max-[1280px]:w-[95%] max-[1023px]:w-full max-[1023px]:flex-col max-[1023px]:items-center max-[768px]">
+          <div className="mr-[140px] max-[1350px]:mr-[80px] max-[1023px]:mr-0 max-[1023px]:flex max-[1023px]:flex-col max-[1023px]:items-center max-[1023px]:mb-[50px]">
             <Image
               src="/assets/JPG-02.png"
               alt="steady flights logo"
@@ -17,72 +17,72 @@ export const Footer = () => {
               Flights you need, at prices you&apos;ll love.
             </p>
           </div>
-          <div className="flex">
-            <div className="flex">
-              <div className="flex">
+          <div className="flex max-[768px]:w-full max-[768px]:justify-center">
+            <div className="flex max-[768px]:w-full max-[768px]:flex-col-reverse max-[768px]:items-center">
+              <div className="flex max-[768px]:flex-wrap max-[768px]:ml-[30px] max-[768px]:justify-between">
                 <div className="mr-[50px]">
-                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular']">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular'] max-[768px]:mb-[24px]">
                     Company
                   </h1>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     About us
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Career
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Blog
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Tickets
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Pricing
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Demo
                   </p>
                 </div>
-                <div className="mr-[50px]">
-                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular']">
+                <div className="mr-[50px] max-[768px]:mr-[50px]">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular'] max-[768px]:mb-[24px]">
                     Support
                   </h1>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Help Center
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Contact us
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Terms & Condition
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Sitemap
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Feedback
                   </p>
                 </div>
-                <div className="mr-[50px]">
-                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular']">
+                <div className="mr-[50px] max-[768px]:mr-[30px] max-[390px]:mt-[25px]">
+                  <h1 className="text-[14px] text-white mb-[44px] tracking-[0.2px] font-['customFontRegular'] max-[768px]:mb-[24px]">
                     Media
                   </h1>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Facebook
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Twitter
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Youtube
                   </p>
-                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular']">
+                  <p className="text-[12px] text-[#e3e4e4] mb-[18px] cursor-pointer font-['customFontRegular'] max-[768px]:mb-[12px]">
                     Instagram
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col">
-                <h1 className="text-[14px] text-white mb-[33px] font-['customFontRegular']">
+              <div className="flex flex-col max-[768px]:w-full max-[768px]:items-center">
+                <h1 className="text-[14px] text-white mb-[33px] font-['customFontRegular'] max-[768px]:text-center max-[768px]:mb-[15px]">
                   Subscribe Us
                 </h1>
                 <input
@@ -90,9 +90,9 @@ export const Footer = () => {
                   name="subscribe"
                   id="subscribe"
                   placeholder="Your email here..."
-                  className="w-[250px] h-[61px] text-white rounded-none rounded-[7px] text-[12px] font-['customFontRegular'] outline-none pl-[25px] pr-[25px] bg-[#373B3E] mb-[15px] placeholder:text-white"
+                  className="w-[250px] h-[61px] text-white rounded-none rounded-[7px] text-[12px] font-['customFontRegular'] outline-none pl-[25px] pr-[25px] bg-[#373B3E] mb-[15px] placeholder:text-white max-[768px]:w-[95%]"
                 />
-                <button className="w-[250px] h-[61px] rounded-[7px] text-[14px] bg-white font-['customFontRegular']">
+                <button className="w-[250px] h-[61px] rounded-[7px] text-[14px] bg-white font-['customFontRegular'] max-[768px]:w-[95%] max-[768px]:mb-[50px]">
                   Subscribe
                 </button>
               </div>
--- components/LoginAndRegister/FormSection.js
+++ components/LoginAndRegister/FormSection.js
@@ -12,130 +12,132 @@ export const FormSection = ({ isRegisterPage }) => {
   };
 
   return (
-    <div
-      className={
-        isRegisterPage
-          ? "w-full h-[665px] min-w-[1280px]  relative flex justify-center overflow-x-hidden bg-background-loginSignupForm-image bg-no-repeat bg-center bg-cover"
-          : "w-full h-[665px] min-w-[1280px] relative flex justify-center overflow-x-hidden bg-background-loginSignupForm-image bg-no-repeat bg-center bg-cover"
-      }
-    >
-      <div className="w-full max-w-[1366px] flex justify-center">
-        <div className="w-[73.5%]">
-          <div
-            className={
-              isRegisterPage
-                ? "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[10px] mt-[60px]"
-                : "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[12px] mt-[80px]"
-            }
-          >
-            <h1
+    <div className="overflow-x-hidden">
+      <div
+        className={
+          isRegisterPage
+            ? "w-full h-[665px] min-w-[1280px] relative flex justify-center  bg-background-loginSignupForm-image bg-no-repeat bg-center bg-cover"
+            : "w-full h-[665px] min-w-[1280px] relative flex justify-center  bg-background-loginSignupForm-image bg-no-repeat bg-center bg-cover"
+        }
+      >
+        <div className="w-full max-w-[1366px] flex justify-center">
+          <div className="w-[73.5%]">
+            <div
               className={
                 isRegisterPage
-                  ? "text-[20px] mb-[6px] text-[#212529] font-['customFontExtraBold']"
-                  : "text-[20px] mb-[10px] text-[#212529] font-['customFontExtraBold']"
+                  ? "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[10px] mt-[60px]"
+                  : "w-[464px] bg-white rounded-[10px] border-t-[4px] border-solid border-t-[#1ABEC8] pt-[30px] px-[35px] pb-[12px] mt-[80px]"
               }
             >
-              <span className="text-[#1ABEC8]">
-                {isRegisterPage ? "Register" : "Login"}
-              </span>{" "}
-              {isRegisterPage
-                ? "to fly away with us!"
-                : "and let's explore world!"}
-            </h1>
-            <p className="text-[12px] mb-[27px] font-['customFontMedium']">
-              {isRegisterPage
-                ? "Flights you need, at prices you'll love."
-                : "At prices you'll love."}
-            </p>
-            <form>
-              <div className="w-full h-[61px] border border-solid border-[#D3D3D3] rounded-[15px] px-[15px]">
-                <p className="text-[#A6A8A9] text-[12px] mt-[8px] font-['customFontMedium']">
-                  Email{isRegisterPage && "*"}
-                </p>
-                <input
-                  type="text"
-                  name="Email"
-                  id="Email"
-                  autoComplete="off"
-                  className="w-full text-[13px] text-[#212529] border-none outline-none mt-[5px] font-['customFontBold']"
-                />
-              </div>
-              <div className="w-full h-[61px] border border-solid border-[#D3D3D3] rounded-[15px] px-[15px] flex justify-between items-center mt-[17px]">
-                <div className="w-full h-full">
+              <h1
+                className={
+                  isRegisterPage
+                    ? "text-[20px] mb-[6px] text-[#212529] font-['customFontExtraBold']"
+                    : "text-[20px] mb-[10px] text-[#212529] font-['customFontExtraBold']"
+                }
+              >
+                <span className="text-[#1ABEC8]">
+                  {isRegisterPage ? "Register" : "Login"}
+                </span>{" "}
+                {isRegisterPage
+                  ? "to fly away with us!"
+                  : "and let's explore world!"}
+              </h1>
+              <p className="text-[12px] mb-[27px] font-['customFontMedium']">
+                {isRegisterPage
+                  ? "Flights you need, at prices you'll love."
+                  : "At prices you'll love."}
+              </p>
+              <form>
+                <div className="w-full h-[61px] border border-solid border-[#D3D3D3] rounded-[15px] px-[15px]">
                   <p className="text-[#A6A8A9] text-[12px] mt-[8px] font-['customFontMedium']">
-                    Password{isRegisterPage && "*"}
+                    Email{isRegisterPage && "*"}
                   </p>
                   <input
                     type="text"
-                    name="Password"
-                    id="Password"
+                    name="Email"
+                    id="Email"
                     autoComplete="off"
                     className="w-full text-[13px] text-[#212529] border-none outline-none mt-[5px] font-['customFontBold']"
                   />
                 </div>
-                <Image
-                  src="/assets/eye 1.svg"
-                  alt="hide password"
-                  width={20}
-                  height={21}
-                  className="cursor-pointer"
-                />
-              </div>
-            </form>
-            {isRegisterPage ? (
-              <div className="text-[12px] text-[#7A7C7F] inline-block mt-[11px] mb-[17px] font-['customFontMedium']">
-                Password must be at least 8 characters.
-              </div>
-            ) : (
-              <div className="text-[12px] inline-block cursor-pointer my-[12px] tracking-[0.2px] font-['customFontBold']">
-                Reset Password
-              </div>
-            )}
-            {isRegisterPage && (
-              <div>
-                <div
-                  className="inline-flex items-center mb-[13px]"
-                  onClick={handleClickCheckbox}
-                >
-                  <div className="w-[14px] h-[14px] rounded-[3px] bg-[#ebebeb] block"></div>
-                  <div className="w-[14px] h-[14px] bg-[#1ABEC8] rounded-[3px] relative hidden">
-                    <div className="w-[4.3px] h-[10px] absolute top-px left-[5px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
-                    <div className="w-[4.3px] h-[10px] absolute top-[1.4px] left-[5.4px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
+                <div className="w-full h-[61px] border border-solid border-[#D3D3D3] rounded-[15px] px-[15px] flex justify-between items-center mt-[17px]">
+                  <div className="w-full h-full">
+                    <p className="text-[#A6A8A9] text-[12px] mt-[8px] font-['customFontMedium']">
+                      Password{isRegisterPage && "*"}
+                    </p>
+                    <input
+                      type="text"
+                      name="Password"
+                      id="Password"
+                      autoComplete="off"
+                      className="w-full text-[13px] text-[#212529] border-none outline-none mt-[5px] font-['customFontBold']"
+                    />
                   </div>
-                  <p className="text-[14px] cursor-default ml-[5px] font-['customFontMedium']">
-                    Subscribe to Newsletter
-                  </p>
+                  <Image
+                    src="/assets/eye 1.svg"
+                    alt="hide password"
+                    width={20}
+                    height={21}
+                    className="cursor-pointer"
+                  />
                 </div>
+              </form>
+              {isRegisterPage ? (
+                <div className="text-[12px] text-[#7A7C7F] inline-block mt-[11px] mb-[17px] font-['customFontMedium']">
+                  Password must be at least 8 characters.
+                </div>
+              ) : (
+                <div className="text-[12px] inline-block cursor-pointer my-[12px] tracking-[0.2px] font-['customFontBold']">
+                  Reset Password
+                </div>
+              )}
+              {isRegisterPage && (
+                <div>
+                  <div
+                    className="inline-flex items-center mb-[13px]"
+                    onClick={handleClickCheckbox}
+                  >
+                    <div className="w-[14px] h-[14px] rounded-[3px] bg-[#ebebeb] block"></div>
+                    <div className="w-[14px] h-[14px] bg-[#1ABEC8] rounded-[3px] relative hidden">
+                      <div className="w-[4.3px] h-[10px] absolute top-px left-[5px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
+                      <div className="w-[4.3px] h-[10px] absolute top-[1.4px] left-[5.4px] none border-b border-solid border-b-white border-r border-r-white rotate-[45deg]"></div>
+                    </div>
+                    <p className="text-[14px] cursor-default ml-[5px] font-['customFontMedium']">
+                      Subscribe to Newsletter
+                    </p>
+                  </div>
+                </div>
+              )}
+              <div className="flex flex-col">
+                <button className="w-full h-[44px] bg-[#1ABEC8] text-white text-[14px] border-none rounded-[11px] mb-[10px] tracking-[0.2px] font-['customFontMedium']">
+                  {isRegisterPage ? "Register" : "Login"} with Steady Flights
+                </button>
+                <button className="w-full h-[44px] bg-white text-[14px] border-none rounded-[11px] flex justify-center items-center text-[#0000008A] shadow-[0px_2px_3px_0px_#0000002B] mb-[20px] font-['customFontBold']">
+                  <Image
+                    src="/assets/logo googleg 48dp.png"
+                    alt="google authorization"
+                    width={24}
+                    height={24}
+                    className="mr-[12px]"
+                  />
+                  {isRegisterPage ? "Register" : "Login"} with Google
+                </button>
               </div>
-            )}
-            <div className="flex flex-col">
-              <button className="w-full h-[44px] bg-[#1ABEC8] text-white text-[14px] border-none rounded-[11px] mb-[10px] tracking-[0.2px] font-['customFontMedium']">
-                {isRegisterPage ? "Register" : "Login"} with Steady Flights
-              </button>
-              <button className="w-full h-[44px] bg-white text-[14px] border-none rounded-[11px] flex justify-center items-center text-[#0000008A] shadow-[0px_2px_3px_0px_#0000002B] mb-[20px] font-['customFontBold']">
-                <Image
-                  src="/assets/logo googleg 48dp.png"
-                  alt="google authorization"
-                  width={24}
-                  height={24}
-                  className="mr-[12px]"
-                />
-                {isRegisterPage ? "Register" : "Login"} with Google
-              </button>
+              <p className="text-[12px] tracking-[0.2px] font-['customFontBold']">
+                Don&apos;t have an account?{" "}
+                <span
+                  className="text-[#1ABEC8] cursor-pointer"
+                  onClick={
+                    isRegisterPage
+                      ? () => router.replace("/login")
+                      : () => router.replace("/register")
+                  }
+                >
+                  {isRegisterPage ? "Log in" : "Register"}
+                </span>
+              </p>
             </div>
-            <p className="text-[12px] tracking-[0.2px] font-['customFontBold']">
-              Don&apos;t have an account?{" "}
-              <span
-                className="text-[#1ABEC8] cursor-pointer"
-                onClick={
-                  isRegisterPage
-                    ? () => router.replace("/login")
-                    : () => router.replace("/register")
-                }
-              >
-                {isRegisterPage ? "Log in" : "Register"}
-              </span>
-            </p>
           </div>
         </div>
       </div>
